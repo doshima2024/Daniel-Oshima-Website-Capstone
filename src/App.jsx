9d35cbf2ca923ac940865fef4f94d9fb901aebaa
Daniel Oshima <doshima2013@gmail.com>
2025-02-26 12:41:38 -0500

    write ErrorPage component and add to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import SongDisplay from "./components/SongDisplay";
 import Contact from "./components/Contact";
 import NavBar from "./components/NavBar";
+import ErrorPage from "./components/ErrorPage";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 
 function App() {
@@ -14,6 +15,7 @@ return (
       <Route path="/" element={<Home />} />
       <Route path="/songs" element={<SongDisplay />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<ErrorPage />} />
     </Routes>
   </Router>
 )}
---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ErrorPage() {
+    return(
+        <div>
+            <h1>The page you're searching for does not exist.</h1>
+            <p>Please us the menu above to navigate back to the main site. Thank you!</p>
+        </div>
+    )
+}
+
+export default ErrorPage;
