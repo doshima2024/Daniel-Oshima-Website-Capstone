5eb4c242fd3b916a51701259f5861b664c2bdb99
Daniel Oshima <doshima2013@gmail.com>
2025-02-26 11:25:20 -0500

    add NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "./components/Home";
 import SongDisplay from "./components/SongDisplay";
+import NavBar from "./components/NavBar";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 
 function App() {
 
 return (
   <Router>
+    <NavBar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/songs" element={<SongDisplay />} />
